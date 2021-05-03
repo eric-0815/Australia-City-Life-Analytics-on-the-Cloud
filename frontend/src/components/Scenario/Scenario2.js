4765bd76eb83cd9369de29df1ee35cf3ca218e1d
lengary1110 <ziyuanx1@student.unimelb.edu.au>
2021-05-03 10:37:57 +0800

    CCC-3: add tableau api

--- frontend/src/components/Scenario/Scenario2.js
+++ frontend/src/components/Scenario/Scenario2.js
@@ -1,24 +1,55 @@
-import React, { useState, useEffect } from "react";
-import * as apis from '../../apis/apis';
+import React, {useRef, useEffect} from "react";
+import Description from "../Dashboard/Description";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+const { tableau } = window;
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+
+  paper: {
+    marginTop: theme.spacing(5),
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 650,
+  },
+}));
 const Scenario2 = () => {
-  const [scenarioData, setScenarioData] = useState();
+
+  const scenario2classes = useStyles();
+
+  const url = "https://public.tableau.com/views/LearnEmbeddedAnalytics/SalesOverviewDashboard";
+
+  const ref = useRef(null);
+
+  const options = {
+    device: "desktop",
+  }
+  const initViz = () => {
+    new tableau.Viz(ref.current, url, options);
+  }
+
   useEffect(() => {
-    const fetchData = async () => {
-      const scenarioResponse = await apis.getScenarioTwo();
-      if (scenarioResponse.status === 200) {
-        setScenarioData(scenarioResponse.data.row_number);
-      }
-    };
-    fetchData();
-  }, []);
+    initViz();
+  }, [])
 
   return(
     <div>
-      Scenario 2
-      <div>
-        {scenarioData}
-      </div>
+      <div ref ={ref} ></div>
+      <Paper className={scenario2classes.paper}>
+        <Description />
+      </Paper>
     </div>
   )
 }
--- frontend/src/components/Scenario/Scenario3.js
+++ frontend/src/components/Scenario/Scenario3.js
@@ -1,34 +1,90 @@
-import React from "react";
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
+import React, { useState, useEffect } from "react";
+import * as apis from '../../apis/apis';
+import Wordcloud from "../Chart/WordCloud"
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Tasks from '../Dashboard/Tasks';
+import Description from '../Dashboard/Description';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
 
-const MyMapComponent = withScriptjs(
-  withGoogleMap(props => {
-    return (
-      <GoogleMap
-        defaultZoom={5}
-        defaultCenter={{ lat: -37.872603996110286, lng: 145.07584419726558 }}
-      >
-        <Marker position={{ lat: -37.872603996110286, lng: 145.07584419726558 }} />
-      </GoogleMap>
-    );
-  })
-);
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
 
-const Scenario3 = () => (
-  <div>
-      <MyMapComponent
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBt7V8flUvWd_DIfVwCcNlBFvCDJc93hgI&v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `100vh` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
-  </div>
-)
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 650,
+  },
+}));
+
+const Scenario3 = () => {
+  const scenario1classes = useStyles();
+  const [scenarioData, setScenarioData] = useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      const scenarioResponse = await apis.getScenarioTwo();
+      if (scenarioResponse.status === 200) {
+        setScenarioData(scenarioResponse.data.row_number);
+      }
+    };
+    fetchData();
+  }, []);
+  const fixedHeightPaper = clsx(scenario1classes.paper, scenario1classes.fixedHeight);
+  return(
+    <div>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={8} lg={9}>
+          <Paper className={fixedHeightPaper}>
+            <Wordcloud />
+            <div>
+              {scenarioData}
+            </div>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={4} lg={3}>
+          <Paper className={fixedHeightPaper}>
+            <Tasks />
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={scenario1classes.paper}>
+            <Description />
+          </Paper>
+        </Grid>
+      </Grid>
+      <Box pt={4}>
+        <Copyright />
+      </Box>
+    </div>
+  )
+}
 
 export default Scenario3;
